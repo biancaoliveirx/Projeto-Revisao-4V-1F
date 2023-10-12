dec4488b5dcc7d2d8aa73f7b4b170159d10c1f21
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2023-10-12 17:02:35 -0300

    setup_inicial_classeDistancia_CSV

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import classes.Distancia;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Distancia distancia = new Distancia();
+
+        System.out.println(distancia.calculaDistancia("CAMPO GRANDE", "RIO DE JANEIRO"));
+
+    }
+
+}
