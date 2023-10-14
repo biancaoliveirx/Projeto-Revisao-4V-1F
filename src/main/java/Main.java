2db0f096af5b82fe31d4a0a1436b355a5c41f1e9
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-10-13 23:15:33 -0300

    tirando os comentarios da Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,15 @@ import classes.Caminhoes;
 import classes.Distancia;
 import classes.Menu;
 
+import java.io.File;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Menu menu = new Menu();
         menu.exibirMenu();
-
-        Distancia distancia = new Distancia();
-
-        //System.out.println(distancia.calculaDistancia("CAMPO GRANDE", "RIO DE JANEIRO"));
-
-        Caminhoes caminhoes = new Caminhoes();
-        System.out.println(caminhoes.getCapacidadeMaximaTonelada("pequeno"));
-        System.out.println(caminhoes.getCapacidadeMaximaTonelada("medio"));
-        System.out.println(caminhoes.getCapacidadeMaximaTonelada("grande"));
-        System.out.println(caminhoes.getCapacidadeMaximaTonelada("a"));
 
     }
 }
