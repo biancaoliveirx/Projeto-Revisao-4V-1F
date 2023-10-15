76831d370ecd1a819306bd541cbe15c734b76645
Karoline <karol.v.0570@gmail.com>
2023-10-14 23:25:47 -0300

    Classe Distancia atualizada!

--- src/main/java/classes/Caminhoes.java
+++ src/main/java/classes/Caminhoes.java
@@ -8,14 +8,14 @@ public class Caminhoes {
     private Map<String, Double> precoPorKm = new HashMap<>();
 
     public Caminhoes() {
-        // Inicialize os preços por quilômetro para cada tipo de caminhão
+        //preços por km para cada tipo de caminhão
         precoPorKm.put("pequeno", 5.83);
         precoPorKm.put("medio", 13.42);
         precoPorKm.put("grande", 29.21);
     }
 
     public double getPrecoPorKm(String tipoCaminhao) {
-        // Verifique se o tipo de caminhão existe no mapa.
+        //verifica se o tipo de caminhão existe no mapa
         if (precoPorKm.containsKey(tipoCaminhao)) {
             return precoPorKm.get(tipoCaminhao);
         } else {
